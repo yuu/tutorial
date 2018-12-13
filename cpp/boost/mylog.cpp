3ec249a98550272708e9ec307e872a8d30486097
yuu <yuu@users.noreply.github.com>
2018-12-13 10:32:08 +0900

    cpp: sink that stores log records into a text file

--- cpp/boost/mylog.cpp
+++ cpp/boost/mylog.cpp
@@ -1,5 +1,7 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
 namespace logging = boost::log;
 namespace sinks = boost::log::sinks;
@@ -9,12 +11,16 @@ namespace attrs = boost::log::attributes;
 namespace keywords = boost::log::keywords;
 
 void init() {
+    logging::add_file_log(
+        keywords::format = "[%TimeStamp%]: %Message%");
     logging::core::get()->set_filter(
         logging::trivial::severity >= logging::trivial::info);
 }
 
 int main() {
     init();
+    logging::add_common_attributes();
+
     BOOST_LOG_TRIVIAL(trace) << "A trace servity message";
     BOOST_LOG_TRIVIAL(info) << "A info servity message";
     return 0;
