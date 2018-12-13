df9ef93581862d4f1ddd6f276e104403216f28c2
yuu <yuu@users.noreply.github.com>
2018-12-13 16:12:04 +0900

    cpp: remove trivial msg

--- cpp/boost/mylog.cpp
+++ cpp/boost/mylog.cpp
@@ -1,27 +1,5 @@
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-
-namespace logging = boost::log;
-namespace sinks = boost::log::sinks;
-namespace src = boost::log::sources;
-namespace expr = boost::log::expressions;
-namespace attrs = boost::log::attributes;
-namespace keywords = boost::log::keywords;
-
-void init() {
-    logging::add_file_log(
-        keywords::format = "[%TimeStamp%]: %Message%");
-    logging::core::get()->set_filter(
-        logging::trivial::severity >= logging::trivial::info);
-}
 
 int main() {
-    init();
-    logging::add_common_attributes();
 
-    BOOST_LOG_TRIVIAL(trace) << "A trace servity message";
-    BOOST_LOG_TRIVIAL(info) << "A info servity message";
     return 0;
 }
